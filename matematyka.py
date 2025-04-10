23c597ee0519ab7033ccf9a1c4f71edb67b18b06
WG <wojciech.gryk@ergohestia.pl>
2025-04-10 15:47:45 +0200

    matematyka

---
+++ matematyka.py
@@ -0,0 +1,19 @@
+number1 = 1
+number2 = 2
+number3 = 3.0
+
+result = number1 + number2 +3
+print("sum",result)
+
+result = number1 - number2 - 10
+print("odejmowanie",result)
+
+result = number1 * 7
+print("mnozenie",result)
+
+result = number1 / 3
+print("dzielenie",result)
+
+
+
+
--- test.py
+++ test.py
@@ -3,5 +3,4 @@ liczba_float = 1.0
 typ1 = type(liczba)
 typ2 = type(liczba_float)
 
-
 print(typ1)
