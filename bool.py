8ca9b2746ed6bd6bb19a1cfe68b7a8c5075fd7d7
WG <wojciech.gryk@ergohestia.pl>
2025-04-15 14:36:29 +0200

    bool

---
+++ bool.py
@@ -0,0 +1,8 @@
+bool_true = True
+bool_false = False
+null_value = None
+
+if bool_true:
+    print("prawda")
+else:
+    print("fałsz")
---
+++ bool2.py
@@ -0,0 +1,13 @@
+text1 = "test"
+text2 = ""
+number1 = 0
+number2 = 2
+number3 = -1
+float1 = 0.0
+float2 = 1.3
+
+print(text1, bool(text1))
+print(text2, bool(text2))
+print(number1, bool(number1))
+print(number2, bool(number2))
+print(number3, bool(number3))
