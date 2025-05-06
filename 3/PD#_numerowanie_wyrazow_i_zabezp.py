d32366a885c6d34eec7566282f717a6a8c567841
WG <wojciech.gryk@ergohestia.pl>
2025-05-06 14:28:06 +0200

    PD3 cz 4

--- 3/PD#_numerowanie_wyrazow_i_zabezp.py
+++ 3/PD#_numerowanie_wyrazow_i_zabezp.py
@@ -2,10 +2,10 @@
 # oraz wieloma spacjami na początku i końcu tekstu
 
 tekst = input("wprowadź tekst z kilku wyrazów: ")
-tekst1 = ' '.join(tekst.split())
+#tekst1 = ' '.join(tekst.split())
 
-slowa = tekst1.split(sep=" ")
+slowa = tekst1.split()
 i = 0
 
 for i, wyraz in enumerate(slowa):
-    print(i + 1, ":", wyraz)
+    print(f"{i + 1}: {wyraz})
